a50ff37b5132f05d5f342f078e2dbd84f8197675
Igor Kling <klingiv@altarix.ru>
2019-08-06 22:33:44 +0400

    add pixi container

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
 
-render(<App />, document.getElementById('app'))
+render(<App />, document.getElementById('lobby'))
+
+//Create a Pixi Application
+let app = new PIXI.Application({
+  width: 256, // default: 800
+  height: 256, // default: 600
+  antialias: true, // default: false
+  transparent: false, // default: false
+  resolution: 1 // default: 1
+})
+document.getElementById('game').appendChild(app.view)
