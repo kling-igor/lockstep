05e635a345c608dde1be88fdb3addb2a736cc683
Igor Kling <klingiv@altarix.ru>
2019-08-06 21:58:08 +0400

    initial commit

---
+++ src/client/webpack.config.babel.js
@@ -0,0 +1,50 @@
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import { join } from 'path'
+
+module.exports = env => ({
+  entry: join(__dirname, 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: join(__dirname, '../../app')
+  },
+
+  watch: true,
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: env.dev ? 'source-map' : false,
+
+  resolve: {
+    modules: [__dirname]
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: join(__dirname, 'index.html'),
+      inject: 'body',
+      hash: true
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [__dirname],
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.css']
+  }
+})
---
+++ src/server/main.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import http from 'http'
+import morgan from 'morgan'
+import cors from 'cors'
+// import { join, resolve, basename, parse } from 'path'
+import SocketIO from 'socket.io'
+
+const app = express()
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.static(__dirname + '/'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const httpServer = http.createServer(app)
+httpServer.on('error', e => {
+  if (e.code === 'EADDRINUSE') {
+    httpServer.close()
+  }
+})
+
+let io = SocketIO(httpServer)
+io.on('connection', socket => {
+  console.log('a user connected:', socket.id)
+  // connectedSockets[socket.id] = socket
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+    // delete connectedSockets[socket.id]
+  })
+})
+
+httpServer.listen(8888, () => {})
